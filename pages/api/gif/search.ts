315753bdbafb75d946c8af78bad7654b2df47b29
Glauber Magalhães <glauber.tecinfo@gmail.com>
2024-02-05 14:58:24 -0300

    refactor API and pagination

--- pages/api/gif/search.ts
+++ pages/api/gif/search.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 
@@ -38,7 +37,7 @@ export default async function handler(
       },
     });
 
-    if (!existingSearch && q) {
+    if (!existingSearch && parseInt(offset as string, 10) === 0 && q) {
       await prisma.searchHistory.create({
         data: {
           query: q as string,
